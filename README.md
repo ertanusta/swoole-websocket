ed1d0dc8b3c6b9febfb2f6151868c2f1832f2b34
ertanusta <ertanusta@outlook.com>
2023-10-01 16:05:00 +0300

    readme and edit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Swoole Websocket ile Redis PUB-SUB
+
+Repo içerisinde Redis PUB/SUB kullanılarak Swoole websocket örneği gerçekleştirilmiştir.
--- public/subscription.php
+++ public/subscription.php
@@ -8,17 +8,9 @@ use OpenSwoole\Table;
 use OpenSwoole\Coroutine as Co;
 
 require __DIR__ . '/../vendor/autoload.php';
-Co::set(['hook_flags' => OpenSwoole\Runtime::HOOK_ALL]);
 
 $server = new Server("0.0.0.0", 9502);
 
-$server->on("Start", function (Server $server) {
-    echo "OpenSwoole WebSocket Server is started at http://127.0.0.1:9502\n";
-});
-
-$server->on('Open', function (Server $server, Request $request) {
-    echo "connection open: {$request->fd}\n";
-});
 $redis = new Redis(['host' => '127.0.0.1', 'port' => 6379, 'readTimeout' => 0, 'connectTimeout' => 0, 'persistent' => true]);
 $redis->setOption(Redis::OPT_READ_TIMEOUT,-1);
 $server->on('Message', function (Server $server, Frame $frame) use ($redis) {
@@ -27,15 +19,6 @@ $server->on('Message', function (Server $server, Frame $frame) use ($redis) {
     $redis->subscribe([$channel], function ($redis, $channel, $message) use ($server, $frame) {
         $server->push($frame->fd, $message);
     });
-});
-
-$server->on('Close', function (Server $server, int $fd) {
-    echo "connection close: {$fd}\n";
-
-});
-
-$server->on('Disconnect', function (Server $server, int $fd) {
-    echo "connection disconnect: {$fd}\n";
 });
 
 $server->start();
